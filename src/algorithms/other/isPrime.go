216beee7b0a41586e38db85a58a8b2a5f00d9dea
Dmitriy Loskutov <dmitriyeloskutov@gmail.com>
2020-01-19 10:07:15 +0300

    isPrime number

---
+++ src/algorithms/other/isPrime.go
@@ -0,0 +1,14 @@
+package other
+
+// IsPrime - check if number is prime
+func IsPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
---
+++ src/algorithms/other/isPrime_test.go
@@ -0,0 +1,20 @@
+package other
+
+import "testing"
+
+import "github.com/stretchr/testify/assert"
+
+func TestIsPrime(t *testing.T) {
+	assert.Equal(t, true, IsPrime(2))
+	assert.Equal(t, true, IsPrime(3))
+	assert.Equal(t, true, IsPrime(13))
+	assert.Equal(t, true, IsPrime(17))
+	assert.Equal(t, true, IsPrime(233))
+	assert.Equal(t, true, IsPrime(997))
+
+	assert.Equal(t, false, IsPrime(4))
+	assert.Equal(t, false, IsPrime(8))
+	assert.Equal(t, false, IsPrime(81))
+	assert.Equal(t, false, IsPrime(243))
+	assert.Equal(t, false, IsPrime(998))
+}
