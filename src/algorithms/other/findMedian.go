2a7dc3b6d093e3394cbc7a3b81f2f9fbd48c53a3
Dmitriy Loskutov <dmitriyeloskutov@gmail.com>
2020-01-26 00:48:11 +0300

    findMedian

---
+++ src/algorithms/other/findMedian.go
@@ -0,0 +1,51 @@
+package other
+
+import (
+	"math/rand"
+	"time"
+)
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// FindMedian - find median for unsorted array
+func FindMedian(array []float64) float64 {
+	length := len(array)
+	isOdd := (length % 2) == 1
+
+	if isOdd {
+		return quickselect(length/2, array)
+	}
+	return 0.5 * (quickselect(length/2, array) + quickselect(length/2-1, array))
+}
+
+func quickselect(index int, array []float64) float64 {
+	length := len(array)
+	if length == 1 {
+		return array[0]
+	}
+
+	leftArray := make([]float64, 0)
+	rightArray := make([]float64, 0)
+	pivotsArray := make([]float64, 0)
+
+	pivotValue := array[r.Intn(length)]
+
+	for _, value := range array {
+		if pivotValue > value {
+			leftArray = append(leftArray, value)
+		} else if pivotValue < value {
+			rightArray = append(rightArray, value)
+		} else {
+			pivotsArray = append(pivotsArray, value)
+		}
+	}
+
+	if index < len(leftArray) {
+		return quickselect(index, leftArray)
+	}
+	if index < len(leftArray)+len(pivotsArray) {
+		return pivotsArray[0]
+	}
+
+	return quickselect(index-len(leftArray)-len(pivotsArray), rightArray)
+}
---
+++ src/algorithms/other/findMedian_test.go
@@ -0,0 +1,17 @@
+package other
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindMedian(t *testing.T) {
+	assert.Equal(t, 5.0, FindMedian([]float64{1, 2, 9, 4, 5, 3, 7, 8, 6}))
+	assert.Equal(t, 5.5, FindMedian([]float64{7, 3, 5, 6, 8, 9, 4, 1, 2, 6}))
+	assert.Equal(t, 2.0, FindMedian([]float64{5, 2, 1, 4, 2}))
+	assert.Equal(t, 4.0, FindMedian([]float64{1, 5, 5, 1, 4}))
+	assert.Equal(t, 100.0, FindMedian([]float64{1, 1000, 100}))
+	assert.Equal(t, 51.0, FindMedian([]float64{1, 1000, 100, 2}))
+	assert.Equal(t, 5.0, FindMedian([]float64{9, 1, 0, 2, 3, 4, 6, 8, 7, 10, 5}))
+}
