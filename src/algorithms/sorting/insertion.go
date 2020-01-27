64ea415bea21df6ed3d0c050fc7c1f682668fb3c
Dmitriy Loskutov <dmitriyeloskutov@gmail.com>
2020-01-28 01:45:17 +0300

    add insertion and selection sorting algorigthms

---
+++ src/algorithms/sorting/insertion.go
@@ -0,0 +1,12 @@
+package sorting
+
+// Insertion - sort by selection
+func Insertion(array []int64) {
+	length := len(array)
+
+	for i := 0; i < length; i++ {
+		for j := i; j > 0 && array[j-1] > array[j]; j-- {
+			array[j-1], array[j] = array[j], array[j-1]
+		}
+	}
+}
---
+++ src/algorithms/sorting/selection.go
@@ -0,0 +1,16 @@
+package sorting
+
+// Selection - sort by selection
+func Selection(array []int64) {
+	length := len(array)
+
+	for i := 0; i < length; i++ {
+		minIndex := i
+		for j := i + 1; j < length; j++ {
+			if array[minIndex] > array[j] {
+				minIndex = j
+			}
+		}
+		array[minIndex], array[i] = array[i], array[minIndex]
+	}
+}
