a49f09e94e4b6e445d261ec7a9ec980bddcd9986
Dmitriy Loskutov <dmitriyeloskutov@gmail.com>
2020-04-09 00:30:31 +0300

    graph and directed graph

---
+++ src/algorithms/graph/breadthFirstSearch.go
@@ -0,0 +1,30 @@
+package graph
+
+import (
+	"github.com/dloskutov/go-algorithms/src/ds"
+)
+
+// BreadthFirstSearch - check if there is path
+// from vertex with sourceValue to vertex with checkValue
+func BreadthFirstSearch(graph *ds.Graph, sourceValue int64, checkValue int64) bool {
+	marked := make(map[int64]bool, 0)
+
+	sourceVertex := graph.GetVertexByValue(sourceValue)
+	if sourceVertex != nil {
+		verticesToCheck := make([]*ds.Vertex, 0)
+		verticesToCheck = append(verticesToCheck, sourceVertex.AdjacentVertices...)
+
+		i := 0
+		for i < len(verticesToCheck) {
+			vertexToCheck := verticesToCheck[i]
+			if !marked[vertexToCheck.Value] {
+				marked[vertexToCheck.Value] = true
+				verticesToCheck = append(verticesToCheck, vertexToCheck.AdjacentVertices...)
+			}
+			i++
+		}
+
+		return marked[checkValue]
+	}
+	return false
+}
