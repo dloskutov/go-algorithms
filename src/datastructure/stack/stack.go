d9f6de428d3d1dc889e133d300d627389aefc99a
Dmitriy Loskutov <dmitriyeloskutov@gmail.com>
2021-12-05 18:43:05 +0300

    stack implementation

--- src/datastructure/stack/stack.go
+++ src/datastructure/stack/stack.go
@@ -1,18 +1,38 @@
 package stack
 
-type structure struct{}
+import "fmt"
+
+var ErrEmpty = fmt.Errorf("stack is empty")
+
+type structure struct {
+	items []int
+}
 
 func (s *structure) Push(value int) {
-	// @TODO: need to implement
+	s.items = append(s.items, value)
 }
 
 func (s *structure) Pop() (int, error) {
-	// @TODO: need to implement
-	return 0, nil
+	if s.Size() == 0 {
+		return 0, ErrEmpty
+	}
+	value := s.items[s.Size()-1]
+	s.items = s.items[:s.Size()-1]
+	return value, nil
+}
+
+func (s *structure) Size() int {
+	return len(s.items)
 }
 
 func New(elements []int) *structure {
-	s := &structure{}
+	s := &structure{
+		items: make([]int, len(elements)),
+	}
+
+	for i := range elements {
+		s.items[i] = elements[i]
+	}
 
 	return s
 }
--- src/datastructure/stack/stack_test.go
+++ src/datastructure/stack/stack_test.go
@@ -11,3 +11,47 @@ func TestNew(t *testing.T) {
 
 	assert.NotNil(t, s)
 }
+
+func TestPut(t *testing.T) {
+	s := New([]int{1, 2, 3, 4})
+
+	s.Push(1)
+	s.Push(4)
+
+	value, err := s.Pop()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 4, value)
+}
+
+func TestPop(t *testing.T) {
+	s := New(nil)
+
+	value, err := s.Pop()
+	assert.Equal(t, ErrEmpty, err)
+	assert.Equal(t, 0, value)
+
+	s.Push(1)
+	value, err = s.Pop()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, value)
+
+	s.Push(4)
+	s.Push(10)
+	s.Push(11)
+
+	value, err = s.Pop()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 11, value)
+
+	value, err = s.Pop()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 10, value)
+
+	value, err = s.Pop()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 4, value)
+
+	value, err = s.Pop()
+	assert.Equal(t, ErrEmpty, err)
+	assert.Equal(t, 0, value)
+}
