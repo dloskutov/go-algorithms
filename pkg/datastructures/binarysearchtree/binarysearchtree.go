8f6839a3c65abaf319e0c3d5f1b0b75e42ec30c8
Dmitriy Loskutov <dmitriyeloskutov@gmail.com>
2021-12-18 15:03:23 +0300

    Insert/Search methods for Binary Search Tree

--- pkg/datastructures/binarysearchtree/binarysearchtree.go
+++ pkg/datastructures/binarysearchtree/binarysearchtree.go
@@ -1,15 +1,67 @@
 package binarysearchtree
 
-type BinarySearchTree struct{}
+import "fmt"
+
+var ErrInvalidKey = fmt.Errorf("invalid key")
+
+type node struct {
+	key       int
+	value     interface{}
+	leftNode  *node
+	rightNode *node
+}
+
+type BinarySearchTree struct {
+	root *node
+}
 
 func (bst *BinarySearchTree) Put(key int, value interface{}) error {
-	// @TODO: need to implement
+	if bst.root == nil {
+		bst.root = &node{
+			key:   key,
+			value: value,
+		}
+		return nil
+	}
+
+	currentNode := bst.root
+	parentNode := currentNode
+	for currentNode != nil {
+		parentNode = currentNode
+		if key > currentNode.key {
+			currentNode = currentNode.rightNode
+		} else {
+			currentNode = currentNode.leftNode
+		}
+	}
+	if key > parentNode.key {
+		parentNode.rightNode = &node{
+			key:   key,
+			value: value,
+		}
+	} else {
+		parentNode.leftNode = &node{
+			key:   key,
+			value: value,
+		}
+	}
+
 	return nil
 }
 
 func (bst *BinarySearchTree) Search(key int) (interface{}, error) {
-	// @TODO: need to implement
-	return nil, nil
+	parentNode := bst.root
+	for parentNode != nil {
+		if parentNode.key == key {
+			return parentNode.value, nil
+		} else if key > parentNode.key {
+			parentNode = parentNode.rightNode
+		} else {
+			parentNode = parentNode.leftNode
+		}
+	}
+
+	return nil, ErrInvalidKey
 }
 
 func (bst *BinarySearchTree) Remove(key int) error {
--- pkg/datastructures/binarysearchtree/binarysearchtree_test.go
+++ pkg/datastructures/binarysearchtree/binarysearchtree_test.go
@@ -7,8 +7,34 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	bst, err := New(map[int]interface{}{})
+	bst, err := New(map[int]interface{}{
+		10: "first",
+		2:  "second",
+		5:  "third",
+		7:  "forth",
+		12: "fifth",
+	})
 
 	assert.Equal(t, nil, err)
 	assert.NotNil(t, bst)
+}
+
+func TestSearch(t *testing.T) {
+	bst, err := New(map[int]interface{}{
+		10: "first",
+		2:  "second",
+		5:  "third",
+		7:  "forth",
+		12: "fifth",
+	})
+
+	assert.Equal(t, nil, err)
+	assert.NotNil(t, bst)
+
+	value, err := bst.Search(5)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, value, "third")
+
+	_, err = bst.Search(111)
+	assert.Equal(t, ErrInvalidKey, err)
 }
