7d3f008a76604dc7fb49d67395cd241e1cf7d91d
Dmitriy Loskutov <dmitriyeloskutov@gmail.com>
2022-01-11 16:50:03 +0300

    fix stylecode

--- pkg/algorithms/other/sqrt/sqrt.go
+++ pkg/algorithms/other/sqrt/sqrt.go
@@ -6,8 +6,8 @@ func Sqrt(value float64) float64 {
 	if value < 0 {
 		return math.NaN()
 	}
-	var err float64 = 0.000000000000001
-	var temp float64 = value
+	err := 0.000000000000001
+	temp := value
 	for math.Abs(temp-value/temp) > err*value {
 		temp = (value/temp + temp) / 2
 	}
